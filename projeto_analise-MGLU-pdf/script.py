0629ac74cb07e400ea8f950a5dcde1f1f05abfca
LUCAS LOPES <lucaslopesfnance@gmail.com>
2024-09-23 15:06:51 -0300

    Ajustes em alguns caminhos

--- projeto_analise-MGLU-pdf/script.py
+++ projeto_analise-MGLU-pdf/script.py
@@ -12,7 +12,7 @@ for i, pagina in enumerate(arquivo_pdf.pages):
     num_pagina = i + 1
     novo_pdf = pyf.PdfWriter()
     novo_pdf.add_page(pagina)
-    with Path(rf"Python\projeto_analise-MGLU-pdf\paginas\Arquivo Pagina {num_pagina}.pdf").open(mode="wb") as arquivo:
+    with Path(rf"Python/projeto_analise-MGLU-pdf/paginas/Arquivo Pagina {num_pagina}.pdf").open(mode="wb") as arquivo:
         novo_pdf.write(arquivo)
 
 # Juntar varios PDF's em 1
@@ -26,7 +26,7 @@ for num in num_paginas:
         pagina_pdf = pyf.PdfReader(rf"Python\projeto_analise-MGLU-pdf\paginas\Arquivo Pagina {num}.pdf")
     novo_arquivo.add_page(pagina_pdf.pages[0])
 
-with Path(r"Python\projeto_analise-MGLU-pdf\paginas\consolidado.pdf").open(mode="wb") as arquivoConsolidado:
+with Path(r"Python/projeto_analise-MGLU-pdf/paginas/consolidado.pdf").open(mode="wb") as arquivoConsolidado:
     novo_arquivo.write(arquivoConsolidado)
 
 # Juntar 2 PDF's
@@ -34,7 +34,7 @@ with Path(r"Python\projeto_analise-MGLU-pdf\paginas\consolidado.pdf").open(mode=
 pdf_3T_4T = pyf.PdfMerger().append(r"Python\projeto_analise-MGLU-pdf\MGLU_ER_3T20_POR.pdf")
 pdf_3T_4T.append(r"Python\projeto_analise-MGLU-pdf\MGLU_ER_4T20_POR.pdf")
 
-with Path(r"Python\projeto_analise-MGLU-pdf\paginas\pdf_3T_4T.pdf").open(mode="wb") as arquivo:
+with Path(r"Python/projeto_analise-MGLU-pdf/paginas/pdf_3T_4T.pdf").open(mode="wb") as arquivo:
     pdf_3T_4T.write(arquivo)
 
 # Outras manipulações de PDF's
@@ -43,7 +43,7 @@ pdf_consolidado = pyf.PdfMerger()
 pdf_consolidado.append(r"Python\projeto_analise-MGLU-pdf\MGLU_ER_4T20_POR.pdf")
 pdf_consolidado.merge(1, r'Python\projeto_analise-MGLU-pdf\paginas\Arquivo Pagina 1.pdf')
 
-with Path(r"Python\projeto_analise-MGLU-pdf\paginas\comparacao.pdf").open(mode="wb") as arquivo:
+with Path(r"Python/projeto_analise-MGLU-pdf/paginas/comparacao.pdf").open(mode="wb") as arquivo:
     pdf_consolidado.write(arquivo)
 
 pdf_original = pyf.PdfReader(r"Python\projeto_analise-MGLU-pdf\MGLU_ER_3T20_POR.pdf")
@@ -54,7 +54,7 @@ for pagina in pdf_original.pages:
     pagina.rotate(90)
     pdf_rotacionado.add_page(pagina)
 
-with Path(r"Python\projeto_analise-MGLU-pdf\paginas\rotacionado.pdf").open(mode='wb') as arquivo:
+with Path(r"Python/projeto_analise-MGLU-pdf/paginas/rotacionado.pdf").open(mode='wb') as arquivo:
     pdf_rotacionado.write(arquivo)
 
 arq = pyf.PdfReader(r"Python\projeto_analise-MGLU-pdf\MGLU_ER_3T20_POR.pdf")
