6facae35dfe41e579607bf790372c4328942fdce
LUCAS LOPES <lucaslopesfnance@gmail.com>
2024-09-23 18:29:11 -0300

    Initial Commit

--- projeto_integracao-pastas/script.py
+++ projeto_integracao-pastas/script.py
@@ -1,4 +1,20 @@
 from pathlib import Path
+import shutil
 
-print(Path.cwd())
+# print(Path.cwd())
+
 caminho = Path(r'Python/projeto_integracao-pastas/Arquivos_Lojas')
+arquivos = caminho.iterdir()
+for arquivo in arquivos:
+    print(arquivo)
+
+# Criando uma pasta
+Path('Python/projeto_integracao-pastas/Pasta Auxiliar/pasta2').mkdir()
+
+# Criando uma cópia de um arquivo
+
+shutil.copy2(Path("Python/projeto_integracao-pastas/Arquivos_Lojas/201802_Ibirapuera_SP.csv"), Path("Python\projeto_integracao-pastas\Pasta Auxiliar/201802_Ibirapuera_SP.csv"))
+
+# Movendo o arquivo de uma pasta pra outra
+
+shutil.move(Path("Python/projeto_integracao-pastas/Pasta Auxiliar/201802_Ibirapuera_SP.csv"), Path("Python/projeto_integracao-pastas/Pasta Auxiliar/pasta2/201802_Ibirapuera_SP.csv"))
