7b45f0f72729bd97693e8477caceebb68943d52f
LUCAS LOPES <lucaslopesfnance@gmail.com>
2024-09-24 19:43:36 -0300

    commit
    Tentativa de subdividir os arquivos de acordo com o estado

---
+++ projeto_integracao-pastas/script2.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import shutil
+
+caminho = Path(r"Python/projeto_integracao-pastas/Arquivos_Lojas")
+arquivos = caminho.iterdir()
+
+# Dividir os relatórios dos estados de AM, GO, MG, RJ, SP
+
+estados = ["AM", "SP", "GO", "MG", "RJ"]
+
+for estado in estados:
+    Path(rf"Python/projeto_integracao-pastas/Pasta Auxiliar/{estado}").mkdir()
+
+for arquivo in arquivos:
+    nome_arquivo = arquivo.name
+    print(nome_arquivo)
+    if nome_arquivo[-6:-4] == "AM":
+        shutil.copy2(Path(rf"Python/projeto_integracao-pastas/Arquivos_Lojas/{arquivo}"), Path(rf"Python/projeto_integracao-pastas/Pasta Auxiliar/AM/{arquivo}"))
+    elif nome_arquivo[-6:-4] == "_GO":
+        shutil.copy2(Path(rf"Python/projeto_integracao-pastas/Arquivos_Lojas/{arquivo}"), Path(rf"Python/projeto_integracao-pastas/Pasta Auxiliar/GO/{arquivo}"))
+    elif nome_arquivo[-6:-4] == "_MG":
+        shutil.copy2(Path(rf"Python/projeto_integracao-pastas/Arquivos_Lojas/{arquivo}"), Path(rf"Python/projeto_integracao-pastas/Pasta Auxiliar/MG/{arquivo}"))
+    elif nome_arquivo[-6:-4] == "_RJ":
+        shutil.copy2(Path(rf"Python/projeto_integracao-pastas/Arquivos_Lojas/{arquivo}"), Path(rf"Python/projeto_integracao-pastas/Pasta Auxiliar/RJ/{arquivo}"))
+    elif nome_arquivo[-6:-4] == "_SP":
+        shutil.copy2(Path(rf"Python/projeto_integracao-pastas/Arquivos_Lojas/{arquivo}"), Path(rf"Python/projeto_integracao-pastas/Pasta Auxiliar/SP/{arquivo}"))
+    else:
+        pass
+
+
+
