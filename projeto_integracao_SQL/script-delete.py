edc7eebe05364b808bb5d089eaf413bb097a11e8
LUCAS LOPES <lucaslopesfnance@gmail.com>
2024-10-11 13:56:17 -0300

    Script delete
    Script DELETE do CRUD

---
+++ projeto_integracao_SQL/script-delete.py
@@ -0,0 +1,15 @@
+import pyodbc
+
+
+dados_conexao = {"Drivers={'SQLite3 ODBC Driver'}; Server=localhost; Database=chinook.db"}
+
+conexao = pyodbc.connect(dados_conexao)
+cursor = conexao.cursor()
+
+cursor.execute("""
+               DELETE FROM albuns WHERE AlbumID = 2
+               """)
+cursor.commit()
+
+cursor.close()
+conexao.close()
