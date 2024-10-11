2b4975d6d12c7bdc877b06af252ffaff0d40f1a0
LUCAS LOPES <lucaslopesfnance@gmail.com>
2024-10-11 13:55:57 -0300

    Script Update
    Script UPDATE do CRUD

---
+++ projeto_integracao_SQL/script-update.py
@@ -0,0 +1,15 @@
+import pyodbc
+
+
+dados_conexao = {"Drivers={'SQLite3 ODBC Driver'}; Server=localhost; Database=chinook.db"}
+
+conexao = pyodbc.connect(dados_conexao)
+cursor = conexao.cursor()
+
+cursor.execute("""
+    UPDATE customers SET Email='lucaslopes@reidopython.com.br' WHERE CustumerId=10
+""")
+cursor.commit()
+
+cursor.close()
+conexao.close()
