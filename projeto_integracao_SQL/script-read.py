4380d29ffabb585ac320b980c4d653041f866d25
LUCAS LOPES <lucaslopesfnance@gmail.com>
2024-10-11 12:23:32 -0300

    Script Read
    Criação do Script Read do CRUD

---
+++ projeto_integracao_SQL/script-read.py
@@ -0,0 +1,22 @@
+import pyodbc
+import pandas as pd
+
+# print(pyodbc.drivers())
+
+dados_conexao = {"Drivers={SQLite3 ODBC Driver}; Server=localhost; Database=chinook.db"}
+
+conexao = pyodbc.connect(dados_conexao)
+
+cursor = conexao.cursor()
+
+cursor.execute("SELECT * FROM customers")
+valores = cursor.fetchall()
+
+descricao = cursor.description()
+colunas = [tupla[0] for tupla in descricao] # list comprehension
+
+tabela_clientes = pd.DataFrame.from_records(valores, columns=colunas)
+print(tabela_clientes)
+
+cursor.close()
+conexao.close()
