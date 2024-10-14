19766394e1278cfc23c7d658002bfeac9bfe0a68
LUCAS LOPES <lucaslopesfnance@gmail.com>
2024-10-14 13:48:08 -0300

    Ajustes no script-update

--- projeto_integracao_SQL/script-update.py
+++ projeto_integracao_SQL/script-update.py
@@ -1,7 +1,7 @@
 import pyodbc
 
 
-dados_conexao = {"Drivers={'SQLite3 ODBC Driver'}; Server=localhost; Database=chinook.db"}
+dados_conexao = ("Drivers={'SQLite3 ODBC Driver'}; Server=localhost; Database=chinook.db")
 
 conexao = pyodbc.connect(dados_conexao)
 cursor = conexao.cursor()
