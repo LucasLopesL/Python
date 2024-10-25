1591c9f00ad0188a0ebfc85f12ab26270dccf389
LUCAS LOPES <lucaslopesfnance@gmail.com>
2024-10-25 18:42:30 -0300

    Modificação no app

--- projeto_flask_ap2/app.py
+++ projeto_flask_ap2/app.py
@@ -1,18 +1,5 @@
-from flask import Flask, render_template
+from projeto_flask import app
 
-app = Flask(__name__)
 
-# Criação da primeira página
-# Route
-@app.route("/")
-# Função
-def homepage():
-    return render_template("homepage.html")
-
-@app.route("/contatos")
-def contato():
-    return render_template("contatos.html")
-
-# Colocar o site no ar
 if __name__ == "__main__":
    app.run(debug=True)
