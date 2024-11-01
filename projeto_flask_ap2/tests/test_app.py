1c27b7ce6ea86aedab61ee46e21e63c6e0b1d934
LUCAS LOPES <lucaslopesfnance@gmail.com>
2024-11-01 20:01:17 -0300

    update

--- projeto_flask_ap2/tests/test_app.py
+++ projeto_flask_ap2/tests/test_app.py
@@ -5,7 +5,6 @@ from projeto_flask import app
 
 @pytest.fixture
 def client():
-    # Configura o Flask para modo de teste
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
